2f6a435e34bfce0343e1498ef647dc8b024c5336
Jan Cichocki <jan@cichocki.com>
2023-12-04 10:43:25 -0600

    Add Write File To Class Version

--- PyBank/main_class_version.py
+++ PyBank/main_class_version.py
@@ -17,6 +17,7 @@ class BudgetDataAnalyzer:
         self.monthly_changes = []
         self.greatest_increase = ['', 0]
         self.greatest_decrease = ['', 0]
+        self.average_change = 0  # Initialize average_change
 
     def analyze(self):
         """Analyze the budget data for total months, profit/loss, and changes."""
@@ -34,26 +35,39 @@ class BudgetDataAnalyzer:
                     change = current_month_profit_loss - previous_month_profit_loss
                     self.monthly_changes.append(change)
 
-                    # Update greatest increase and decrease
                     if change > self.greatest_increase[1]:
                         self.greatest_increase = [month[0], change]
                     if change < self.greatest_decrease[1]:
                         self.greatest_decrease = [month[0], change]
 
                 previous_month_profit_loss = current_month_profit_loss
+            
+            if self.monthly_changes:
+                self.average_change = sum(self.monthly_changes) / len(self.monthly_changes)
 
     def print_results(self):
         """Print the results of the analysis."""
-        average_change = sum(self.monthly_changes) / len(self.monthly_changes) if self.monthly_changes else 0
         print("Financial Analysis")
         print("----------------------------")
         print(f"Total Months: {self.total_months}")
         print(f"Total: ${self.total_profit_loss}")
-        print(f"Average Change: ${average_change:.2f}")
+        print(f"Average Change: ${self.average_change:.2f}")
         print(f"Greatest Increase in Profits: {self.greatest_increase[0]} (${self.greatest_increase[1]})")
         print(f"Greatest Decrease in Profits: {self.greatest_decrease[0]} (${self.greatest_decrease[1]})")
+
+    def write_results_to_file(self, output_filepath):
+        """Write the analysis results to a text file."""
+        with open(output_filepath, 'w') as file:
+            file.write("Financial Analysis\n")
+            file.write("----------------------------\n")
+            file.write(f"Total Months: {self.total_months}\n")
+            file.write(f"Total: ${self.total_profit_loss}\n")
+            file.write(f"Average Change: ${self.average_change:.2f}\n")
+            file.write(f"Greatest Increase in Profits: {self.greatest_increase[0]} (${self.greatest_increase[1]})\n")
+            file.write(f"Greatest Decrease in Profits: {self.greatest_decrease[0]} (${self.greatest_decrease[1]})\n")
 
 # Creating an instance of the BudgetDataAnalyzer and using it to analyze the data
 analyzer = BudgetDataAnalyzer('budget_data.csv')
 analyzer.analyze()
 analyzer.print_results()
+analyzer.write_results_to_file('financial_analysis_class_version.txt')
